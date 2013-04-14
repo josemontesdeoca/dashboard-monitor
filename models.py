1f5894b4604f0861918b590a85278cb5b2032b47
Jose Montes de Oca <jfmontesdeoca11@gmail.com>
2013-04-13 17:49:01 -0700

    Add models definition file.

---
+++ models.py
@@ -0,0 +1,45 @@
+# Copyright 2013 Jose Montes de Oca. All Rights Reserved.
+
+"""Dashboard Monitor DB model classes."""
+
+__author__ = 'Jose Montes de Oca <jfmontesdeoca11@gmail.com>'
+
+from google.appengine.ext import ndb
+
+
+class User(ndb.Model):
+    """User Model.
+
+    User model used with App Engine's User API.
+    """
+
+    user = ndb.UserProperty(indexed=False)
+    user_id = ndb.StrinProperty()
+    email = ndb.StringProperty()
+    date_created = ndb.DateTimeProperty(auto_now_add=True)
+
+
+class Page(ndb.Model):
+    """Models an specific page to be track."""
+
+    METHODS = [
+        'GET',
+        'POST',
+        'PUT',
+        'HEAD',
+        'DELETE',
+    ]
+
+    user = ndb.KeyProperty(kind=User)
+    url = ndb.StringProperty()
+    title = ndb.StringProperty()
+    method = ndb.StringProperty(default='GET', choices=set(METHODS))
+    date_created = ndb.DateTimeProperty(auto_now_add=True)
+    last_ping = ndb.DateTimeProperty()
+
+
+class Ping(ndb.Model):
+    """Models an specific ping action to a page."""
+    resp_time = ndb.IntegerProperty()
+    resp_code = ndb.IntegerProperty()
+    date = ndb.DateTimeProperty(auto_now_add=True)
